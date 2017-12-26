fb729fc403db44536e79c89df3c15d23bf22af12
Jang <Jang@192.168.25.52>
2017-12-26 23:45:03 +0900

    연식

--- OimProject/src/com/oim/model/ApplicationModel.java
+++ OimProject/src/com/oim/model/ApplicationModel.java
@@ -1,7 +1,5 @@
-package com.oim.model;
 
-import java.util.ArrayList;
-import java.util.List;
+package com.oim.model;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,8 +8,7 @@ import com.oim.application.dao.ApplicationDAO;
 import com.oim.application.dao.ApplicationVO;
 import com.oim.controller.Controller;
 import com.oim.controller.RequestMapping;
-import com.oim.meeting.dao.MeetingDAO;
-import com.oim.meeting.dao.MeetingVO;
+
 
 @Controller
 public class ApplicationModel {
@@ -19,13 +16,28 @@ public class ApplicationModel {
     public String meeting_payment_ok(HttpServletRequest req, HttpServletResponse res) {
 		
 		ApplicationVO vo = new ApplicationVO();
+		
 		String meet_no = req.getParameter("meet_no");
 		String om_id = req.getParameter("om_id");
+		
 		vo.setMeet_no(Integer.parseInt(meet_no));
 		vo.setOm_id(om_id);
 		ApplicationDAO.ApplicationInsertData(vo);
+		System.out.println(meet_no);
+		System.out.println(om_id);
 		
     	req.setAttribute("main_jsp", "../meeting/meeting_detail.jsp");
         return "main/main.jsp";
     }
 }
+
+
+
+
+
+
+
+
+
+
+
